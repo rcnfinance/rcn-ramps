563e9eb2c0842e5ac66b795cc84107a010a5d9b5
rotcivegaf <victorfage@gmail.com>
2018-05-08 14:22:12 -0300

    add function documentation

--- contracts/KyberGateway.sol
+++ contracts/KyberGateway.sol
@@ -16,29 +16,43 @@ contract KyberGateway is RpSafeMath {
         RCN = _RCN;
     }
 
+    /**
+        @notice TODO
+        @dev TODO
+
+        @param _kyber kyverNetwork market
+        @param _rcnEngine the engine of RCN
+        @param _index Index of the loan
+        @param _cosigner Address of the cosigner, 0x0 for lending without cosigner.
+        @param _cosignerData Data required by the cosigner to process the request.
+        @param _oracleData Data required by the oracle to return the rate, the content of this field must be provided
+            by the url exposed in the url() method of the oracle.
+
+        @return true if the trade and lend was done successfully
+    */
     function lend(
-        KyberMock kyber,
-        Engine engine,
-        uint idLoan,
-        Cosigner cosigner,
-        bytes cosignerData,
-        bytes oracleData
+        KyberMock _kyber,
+        Engine _rcnEngine,
+        uint _index,
+        Cosigner _cosigner,
+        bytes _cosignerData,
+        bytes _oracleData
     ) public payable returns (bool) {
-        uint256 loanAmount = engine.getAmount(idLoan);
+        uint256 loanAmount = _rcnEngine.getAmount(_index);
         uint256 rate;
-        (rate, ) = kyber.getExpectedRate(RCN, ETH, loanAmount);
-        uint256 targetAmount = kyber.convertRate(loanAmount, rate);
+        (rate, ) = _kyber.getExpectedRate(RCN, ETH, loanAmount);
+        uint256 targetAmount = _kyber.convertRate(loanAmount, rate);
 
         require(msg.value >= targetAmount, "Insufficient funds");
 
         uint256 returnAmount = safeSubtract(msg.value, targetAmount);
-        uint256 totalTokens = kyber.trade.value(targetAmount)(ETH, targetAmount, RCN, this, 10 ** 30, 0, this);
+        uint256 totalTokens = _kyber.trade.value(targetAmount)(ETH, targetAmount, RCN, this, 10 ** 30, 0, this);
 
-        RCN.approve(address(engine), totalTokens);
+        RCN.approve(address(_rcnEngine), totalTokens);
 
-        require(engine.lend(idLoan, oracleData, cosigner, cosignerData), "fail lend");
+        require(_rcnEngine.lend(_index, _oracleData, _cosigner, _cosignerData), "fail lend");
 
-        require(engine.transfer(msg.sender, idLoan), "fail transfer");
+        require(_rcnEngine.transfer(msg.sender, _index), "fail transfer");
 
         msg.sender.transfer(returnAmount);// if the sender is a contract, the contract needs a fallback function payable
 
