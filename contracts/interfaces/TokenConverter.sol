dd0afe7d70ed0247fda8b3ab6f89ef2d62092674
Agustin Esteban Aguilar <agustin@bitpagos.co>
2018-07-11 22:27:33 +0200

    New Bancor proxy can handle ETH buy/sell

--- contracts/interfaces/TokenConverter.sol
+++ contracts/interfaces/TokenConverter.sol
@@ -4,6 +4,6 @@ import "./../interfaces/Token.sol";
 
 contract TokenConverter {
     address public constant ETH_ADDRESS = 0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee;
-    function getReturn(Token _fromToken, Token _toToken, uint256 _fromAmount) public view returns (uint256 amount);
-    function convert(Token _fromToken, Token _toToken, uint256 _fromAmount, uint256 _minReturn) public payable returns (uint256 amount);
+    function getReturn(Token _fromToken, Token _toToken, uint256 _fromAmount) external view returns (uint256 amount);
+    function convert(Token _fromToken, Token _toToken, uint256 _fromAmount, uint256 _minReturn) external payable returns (uint256 amount);
 }
