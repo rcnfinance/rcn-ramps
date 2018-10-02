fc0bf44610a317c5f7c4e8fac150bfe39769bac5
Joaquin Pablo Gonzalez <joaquin@ripiocredit.network>
2018-10-02 12:01:18 -0300

    Added removeConverter method.

--- contracts/TokenConverterRoute.sol
+++ contracts/TokenConverterRoute.sol
@@ -11,14 +11,32 @@ contract TokenConverterRoute is TokenConverter, Ownable {
     
     uint256 constant internal MAX_UINT = uint256(0) - 1;
     TokenConverter[] private converters;
-    mapping (address => address) private availability;
+    mapping (address => AvailableProvider) private availability;
     
     function addConverter(TokenConverter converter, AvailableProvider availabilityContract) onlyOwner public {
         converters.push(converter);
         availability[converter] = availabilityContract;        
     }
     
-    function converter(Token _from, Token _to, uint256 _amount, uint256 _minReturn) external payable returns (uint256 amount) {
+    function removeConverter(address converter) onlyOwner public returns (bool) {
+        
+        require(converter != address(0), "The address to remove not is available.");
+        require(converters.length > 0, "Not exist element for remove.");
+        
+        for (uint i = 0; i < converters.length; i++) {
+            if (converters[i] == converter) {
+                converters[i] =  converters[converters.length-1];
+                require(1 <= converters.length, "Reverts on overflow.");
+                converters.length--;
+                return true;
+            }
+        }
+        
+        return false;
+        
+    }
+    
+    function convert(Token _from, Token _to, uint256 _amount, uint256 _minReturn) external payable returns (uint256 amount) {
         address betterProxy = _getBetterProxy(_from, _to, _amount);
         TokenConverter converter =  TokenConverter(betterProxy);
         return converter.convert.value(msg.value)(_from, _to, _amount, _minReturn);   
@@ -37,7 +55,7 @@ contract TokenConverterRoute is TokenConverter, Ownable {
         for (uint256 i = 0; i < converters.length; i++) {
             
             TokenConverter converter = TokenConverter(converters[i]);
-            if (_isAvailable(converter, gasleft())) {
+            if (_isAvailable(converter, tx.gasprice)) {
                 
                 uint newRate = converter.getReturn(_from, _to, _amount);
                 if  (newRate > 0 && newRate < minRate) {
@@ -54,7 +72,7 @@ contract TokenConverterRoute is TokenConverter, Ownable {
 
     function _isAvailable(address converter, uint256 _gasPrice) private view returns (bool) {
         
-        if (availability[converter] == 0x0)
+        if (address(availability[converter]) == address(0x0))
             return AvailableProvider(converter).isAvailable(_gasPrice);            
             
         //bancor workaround
