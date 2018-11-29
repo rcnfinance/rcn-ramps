b443a595bce6a4a753356b60045c0d249a2a6e84
rotcivegaf <victorfage@gmail.com>
2018-11-29 20:30:29 -0300

    add safeMath

---
+++ contracts/vendors/rcn/SafeMath.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.24;
+
+library SafeMath {
+    function add(uint256 x, uint256 y) internal pure returns (uint256) {
+        uint256 z = x + y;
+        require(z >= x, "Add overflow");
+        return z;
+    }
+
+    function sub(uint256 x, uint256 y) internal pure returns (uint256) {
+        require(x >= y, "Sub underflow");
+        return x - y;
+    }
+
+    function mult(uint256 x, uint256 y) internal pure returns (uint256) {
+        uint256 z = x * y;
+        require((x == 0)||(z/x == y), "Mult overflow");
+        return z;
+    }
+}
