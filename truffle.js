2fb9e2fca42112bdab7d6a1fca82d7cf68e2299f
rotcivegaf <victorfage@gmail.com>
2018-11-29 11:07:01 -0300

    fix lint

--- truffle.js
+++ truffle.js
@@ -5,34 +5,34 @@ const mnemonic = readFileSync('./mnemonic', 'utf-8');
 const infuraApikey = readFileSync('./infura_access_token', 'utf-8');
 
 module.exports = {
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
-    }
-  },
-  networks: {
-    kyber: {
-      provider: new HDWalletProvider(mnemonic, 'http://localhost:8545', 0, 10),
-      host: 'localhost',
-      port: 8545,
-      network_id: 5777,
-      gas: 6721975,
-      gasPrice: 20000000000,
-      confirmations: 0,
-      timeoutBlocks: 50,
-      skipDryRun: true,
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200,
+        },
     },
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*'
+    networks: {
+        development: {
+            host: 'localhost',
+            port: 8545,
+            network_id: '*'
+        },
+        kyber: {
+            provider: new HDWalletProvider(mnemonic, 'http://localhost:8545', 0, 10),
+            host: 'localhost',
+            port: 8545,
+            network_id: 5777,
+            gas: 6721975,
+            gasPrice: 20000000000,
+            confirmations: 0,
+            timeoutBlocks: 50,
+            skipDryRun: true,
+        },
+        ropsten: {
+            provider: function () {
+                return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraApikey}`);
+            },
+            network_id: 1
+        },
     },
-    ropsten: {
-      provider: function () {
-        return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraApikey}`);
-      },
-      network_id: 1
-    }
-  }
 };
