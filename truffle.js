fa1195c6dda683121452286a36afb281d6d7a534
rotcivegaf <victorfage@gmail.com>
2018-11-29 11:08:05 -0300

    comments lines for local test

--- truffle.js
+++ truffle.js
@@ -1,8 +1,8 @@
-const { readFileSync } = require('fs');
-const HDWalletProvider = require('truffle-hdwallet-provider');
+// const { readFileSync } = require('fs');
+// const HDWalletProvider = require('truffle-hdwallet-provider');
 
-const mnemonic = readFileSync('./mnemonic', 'utf-8');
-const infuraApikey = readFileSync('./infura_access_token', 'utf-8');
+// const mnemonic = readFileSync('./mnemonic', 'utf-8');
+// const infuraApikey = readFileSync('./infura_access_token', 'utf-8');
 
 module.exports = {
     solc: {
@@ -18,7 +18,7 @@ module.exports = {
             network_id: '*'
         },
         kyber: {
-            provider: new HDWalletProvider(mnemonic, 'http://localhost:8545', 0, 10),
+            // provider: new HDWalletProvider(mnemonic, 'http://localhost:8545', 0, 10),
             host: 'localhost',
             port: 8545,
             network_id: 5777,
@@ -30,7 +30,7 @@ module.exports = {
         },
         ropsten: {
             provider: function () {
-                return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraApikey}`);
+                // return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraApikey}`);
             },
             network_id: 1
         },
