26a9f91830ffd1f9840c56ccdea6b08c4192b0bd
rotcivegaf <victorfage@gmail.com>
2018-12-04 11:50:29 -0300

    update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,9 +1,3 @@
-// const { readFileSync } = require('fs');
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-
-// const mnemonic = readFileSync('./mnemonic', 'utf-8');
-// const infuraApikey = readFileSync('./infura_access_token', 'utf-8');
-
 module.exports = {
     solc: {
         optimizer: {
@@ -12,27 +6,17 @@ module.exports = {
         },
     },
     networks: {
+        coverage: {
+            host: 'localhost',
+            network_id: '*', // eslint-disable-line camelcase
+            port: 8545,
+            gas: 0xfffffffffff,
+            gasPrice: 0x01,
+        },
         development: {
             host: 'localhost',
             port: 8545,
-            network_id: '*'
-        },
-        kyber: {
-            // provider: new HDWalletProvider(mnemonic, 'http://localhost:8545', 0, 10),
-            host: 'localhost',
-            port: 8545,
-            network_id: 5777,
-            gas: 6721975,
-            gasPrice: 20000000000,
-            confirmations: 0,
-            timeoutBlocks: 50,
-            skipDryRun: true,
-        },
-        ropsten: {
-            provider: function () {
-                // return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraApikey}`);
-            },
-            network_id: 1
+            network_id: '*', // eslint-disable-line camelcase
         },
     },
 };
